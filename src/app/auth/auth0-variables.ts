1035f1f61ed00cd0ca6a30d9dc1217fd6f2cb10b
brahim <brahim.amezian@hotmail.com>
2019-03-07 14:10:06 +0100

    second

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -3,5 +3,5 @@ import { AuthConfig } from "angular-oauth2-oidc-codeflow-pkce";
 export const AUTH_CONFIG: AuthConfig = {
   clientId: "r5Ur294u5Zxd7bPF4qZUGvnKVogmXcT1",
   issuer: "softwaresecure.eu.auth0.com",
-  redirectUri: "http://localhost:3000/callback",
+  redirectUri: "https://heuristic-leakey-258632.netlify.com/callback"
 };
